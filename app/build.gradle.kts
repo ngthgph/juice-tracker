22e30e8c23f9afd1713066b9fedcac90c500a0ed
ngthgph <ngthgph112@gmail.com>
2024-01-04 16:37:40 +0700

    Add Compose in Gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,10 @@ android {
     buildFeatures {
         viewBinding = true
         dataBinding = true
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
     }
 }
 
@@ -74,4 +78,11 @@ dependencies {
     implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
     implementation("com.google.android.material:material:1.9.0")
     ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
+
+    implementation(platform("androidx.compose:compose-bom:2023.06.01"))
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.compose.material3:material3")
+    implementation("com.google.accompanist:accompanist-themeadapter-material3:0.28.0")
+
+    debugImplementation("androidx.compose.ui:ui-tooling")
 }
